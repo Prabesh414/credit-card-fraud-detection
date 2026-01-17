336444a11dd1d8f4327b6a992f0aa47d3f62ec41
Prabesh Sapkota <prabeshsapkota414@gmail.com>
2026-01-17 14:52:41 +0545

    Remove .next build folder from repository

--- .next/dev/server/pages/api/predict.js
+++
@@ -1,5 +0,0 @@
-var R=require("../../chunks/[turbopack]_runtime.js")("server/pages/api/predict.js")
-R.c("server/chunks/[root-of-the-server]__6b2d01a8._.js")
-R.c("server/chunks/[root-of-the-server]__21eee827._.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/pages-api.js { INNER_PAGE => \"[project]/pages/api/predict.ts [api] (ecmascript)\" } [api] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/pages-api.js { INNER_PAGE => \"[project]/pages/api/predict.ts [api] (ecmascript)\" } [api] (ecmascript)").exports
